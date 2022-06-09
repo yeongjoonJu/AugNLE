05af3c1ca82f0ef9b180e831168a398fec139ffa
yeongjoonJu <yj_ju@korea.ac.kr>
2022-06-09 10:45:10 +0000

    add instruction

---
+++ download.sh
@@ -0,0 +1,13 @@
+wget http://images.cocodataset.org/zips/train2014.zip
+wget http://images.cocodataset.org/zips/val2014.zip
+git clone https://github.com/tylin/coco-caption.git
+mv coco-caption cococaption
+cd cococaption
+./get_stanford_models.sh
+pip install bert_score==0.3.7
+cd ..
+mkdir nle_anno
+gdown https://drive.google.com/drive/folders/16sJjeEQE2o23G-GGUi870ubXzJjdRDua --folder
+mv VQA-X nle_anno/VQA-X
+gdown https://drive.google.com/drive/folders/1b8kUPbgtEduiz8A_VbUg0W_vca7PyXsZ --folder
+mv "cococaption annot"/* cococaption/annotations/
