b5c86cfc96ffcff16462cbe5af5d02659f29753c
yeongjoonJu <yj_ju@korea.ac.kr>
2022-06-10 02:37:38 +0000

    modifying models

--- augnle/models/seq2seq.py
+++ augnle/models/seq2seq.py
@@ -11,37 +11,28 @@ class T5PrefixForConditionalGeneration(T5ForConditionalGeneration):
         self.config = config
         self.tokenizer = tokenizer
         
-        self.prefix_seq_len = config.prefix_seq_len
-        self.n_layers = config.num_hidden_layers#8
-        self.n_heads = config.num_attention_heads#6
+        self.prefix_len = config.prefix_len
+        self.n_layers = config.num_hidden_layers  # 8
+        self.n_heads = config.num_attention_heads # 6
         self.n_embeds = config.hidden_size // config.num_attention_heads#1024/6
-        self.task_name = self.config.task_name
         for param in self.parameters():
             param.requires_grad = False
 
-        self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+        self.dropout = torch.nn.Dropout(config.prefix_dropout)
         self.prefix_encoder = PrefixEncoder(config)
-        self.prefix_tokens = torch.arange(self.prefix_seq_len).long()
+        self.prefix_seqs = [
+            torch.arange(self.prefix_len).unsqueeze(0).long(),
+            torch.arange(self.prefix_len, self.prefix_len*2).unsqueeze(0).long()
+        ]
 
-    def get_prompt(self, batch_size):
-        prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1).to(self.device)#batch,30
-        promt_output = self.prefix_encoder(prefix_tokens)
-        bsz, seq_len, emb_dim = promt_output.shape
-        #promt_output = promt_output.view(bsz, seq_len, self.n_layers*2, self.n_heads, self.n_embeds)
-        promt_output = self.dropout(promt_output)
-        #past_key_values = past_key_values.permute([2,0,3,1,4]).split(2)
+    def get_mixed_prompt(self, batch_size):
+        prefix_tokens1 = self.prefix_seqs[0].expand(batch_size//2,-1).to(self.device)
+        prefix_tokens2 = self.prefix_seqs[1].expand(batch_size-batch_size//2,-1).to(self.device)
+        prefix_tokens = torch.cat((prefix_tokens1, prefix_tokens2), dim=0)
+        prompt_output = self.prefix_encoder(prefix_tokens)
+        prompt_output = self.dropout(prompt_output)
 
-        return promt_output
-    
-    def get_situation_idx(self, ids):
-        situation_token = self.tokenizer.encode("<situation>")[0]
-        situation_idx = []
-        for i in ids:
-            for num,j in enumerate(i):
-                if j == torch.Tensor([situation_token]).to(self.device):
-                    situation_idx.append(num)
-                    break
-        return situation_idx
+        return prompt_output
     
     def forward(self,
                 input_ids=None,
@@ -62,24 +53,17 @@ class T5PrefixForConditionalGeneration(T5ForConditionalGeneration):
         use_cache = use_cache if use_cache is not None else self.config.use_cache
         return_dict = return_dict if return_dict is not None else self.config.return_dict
         
-        batch_size = input_ids.shape[0]
-        prompt_enc = self.get_prompt(batch_size=batch_size)
-        prefix_attention_mask = torch.ones(batch_size, self.prefix_seq_len).to(self.device)
+        batch_size = inputs_embeds.shape[0]
+        prefix_embeds = self.get_mixed_prompt(batch_size=batch_size)
+        prefix_attention_mask = torch.ones(batch_size, self.prefix_len).to(self.device)
+
+        inputs_embeds = torch.cat((prefix_embeds, inputs_embeds), dim=1)
         attention_mask = torch.cat((prefix_attention_mask, attention_mask), dim=1)
-        ids_embeds = self.shared(input_ids)
         
-        # Fill situation embedding
-        if self.task_name == "task_A":
-            sit_idx = self.get_situation_idx(input_ids)
-            for idx,location in enumerate(sit_idx):
-                ids_embeds[idx][location:location+196, :] = inputs_embeds[idx]
-    
-        ids_embeds = torch.cat((prompt_enc,ids_embeds),dim = 1)
         if encoder_outputs is None:
             encoder_outputs = self.encoder(input_ids=None,
                                            attention_mask=attention_mask,
-                                           inputs_embeds=ids_embeds,
-                                           past_key_values=None,
+                                           inputs_embeds=inputs_embeds,
                                            return_dict=return_dict)
             if return_dict:
                 encoder_hidden_states = encoder_outputs.last_hidden_state
@@ -123,8 +107,8 @@ class T5PrefixForConditionalGeneration(T5ForConditionalGeneration):
                                   inputs_embeds=decoder_inputs_embeds,
                                   past_key_values=past_key_values,
                                   attention_mask=decoder_attention_mask,
-                                  encoder_hidden_states=encoder_hidden_states[:,self.prefix_seq_len:],
-                                  encoder_attention_mask=attention_mask[:,self.prefix_seq_len:],
+                                  encoder_hidden_states=encoder_hidden_states,
+                                  encoder_attention_mask=attention_mask,
                                   use_cache=use_cache,
                                   return_dict=return_dict)
 
@@ -147,13 +131,13 @@ class T5PrefixForConditionalGeneration(T5ForConditionalGeneration):
         if labels is not None:
             loss_fct = CrossEntropyLoss(ignore_index=-100)
             loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
-
-        # for training
+        
         if not return_dict:
+            # for training
             pred_lm = torch.argmax(lm_logits, dim=-1)
             outputs = (loss, pred_lm, encoder_hidden_states)
-        # for prediction
         else:
+            # for prediction
             outputs = Seq2SeqLMOutput(
                 loss=loss,
                 logits=lm_logits,
